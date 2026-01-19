8c50724411ec1299e585360d3cb8261365dc5909
Tata <142669125+Dotaka123@users.noreply.github.com>
2026-01-19 15:23:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ async function handlePostback(psid, payload, user) {
 
     // --- INFOS PROXIES AJOUTÉES ICI ---
     if (payload === 'MENU_PAID') {
-        return sendButtons(psid, "🚀 RESIDENTIAL (0 FRAUD SCORE)\n\n• Static ISP: High speed, 1-month duration.\n• Virgin Resi: Never used, perfect for social media.\n• Verizon: Top tier US mobile provider.", [
+        return sendButtons(psid, "🚀 RESIDENTIAL (0 FRAUD SCORE)\n\n• Static ISP: High speed,renewable,many country.\n• Virgin Resi: Never used before.\n• Verizon: best proxy for doing paid survey.", [
             { "title": "Static ISP ($6)", "payload": "BUY_STATIC_6" },
             { "title": "Virgin Resi ($6)", "payload": "BUY_VIRGIN_6" },
             { "title": "Verizon ($4.5)", "payload": "BUY_VERIZON_4.5" }
